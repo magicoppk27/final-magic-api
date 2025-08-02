6eb4f11682ff6f222976a6fb23a85240618979db
magicoppk27 <134200712+magicoppk27@users.noreply.github.com>
2025-08-02 03:06:53 +0100

    Update get-prediction.js

--- api/get-prediction.js
+++ api/get-prediction.js
@@ -1,6 +1,24 @@
+const allowedOrigins = ["https://rodrigo.ie"];
+
 import { kv } from '@vercel/kv';
 
 export default async function handler(req, res) {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  }
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  if (req.method !== "GET") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
   try {
     const prediction = await kv.get('prediction');
     const currentPrediction = prediction || "[No prediction set]";
